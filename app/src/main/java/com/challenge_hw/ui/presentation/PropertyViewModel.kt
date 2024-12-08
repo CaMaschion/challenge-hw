deb75df973dfcaef9ba3cc4ded5e46d48b317ef4
Camila Maschion <cm.maschion@gmail.com>
2024-12-08 20:53:06 +0000

    feat: detail screen

--- app/src/main/java/com/challenge_hw/ui/presentation/PropertyViewModel.kt
+++ app/src/main/java/com/challenge_hw/ui/presentation/PropertyViewModel.kt
@@ -3,6 +3,7 @@ package com.challenge_hw.ui.presentation
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.challenge_hw.data.models.Property
 import com.challenge_hw.data.repository.PropertyRepository
@@ -10,7 +11,10 @@ import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
 
-class PropertyViewModel(private val repository: PropertyRepository) : ViewModel() {
+class PropertyViewModel(
+    savedStateHandle: SavedStateHandle,
+    private val repository: PropertyRepository
+) : ViewModel() {
 
     var propertiesState by mutableStateOf<List<Property>>(emptyList())
         private set
